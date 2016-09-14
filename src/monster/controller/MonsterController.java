16c11f5ba84510c78df93fe792c549b961e97d7e
Benjamin Tschetter <benjamintschetter3@gmail.com>
2016-09-14 12:53:39 -0600

    Added system.out phrases and some other things
    yes

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,9 +1,28 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+
 public class MonsterController 
 {
+	private MarshmallowMonster firstMonster;
+	
+	public MonsterController()
+	{
+		firstMonster = new MarshmallowMonster("KAJ", 4, 2, );
+	}
+	
 	public void start()
 	{
-	
+		System.out.println("We made monsters today!");
+		System.out.println("This is Kareem. I'm so sorry." + firstMonster);
 	}
 }
+
+/*
+ * monster name is Kareem-Abdul Jabbar
+ * has four legs
+ * has no arms
+ * has two eyes
+ * has one antenna
+ *  
+ */
